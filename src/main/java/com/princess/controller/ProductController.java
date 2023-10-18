3eed41d95b0b04a79518f8f2044ea1bf10239085
HoYng <HoYng@Cyclamen>
2023-10-18 18:16:19 +0900

    Entity, HTML

--- src/main/java/com/princess/controller/ProductController.java
+++ src/main/java/com/princess/controller/ProductController.java
@@ -2,7 +2,10 @@ package com.princess.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.princess.domain.Product;
 
 @Controller
 @RequestMapping("/product/")
@@ -10,17 +13,27 @@ public class ProductController {
 
 	@RequestMapping("/getProductList")
 	public void getProductList() {
-		
+
 	}
-	
+
 	@GetMapping("/getProduct")
 	public void getProduct() {
-		
+
 	}
-	
+
 	@GetMapping("/insertProduct")
 	public void insertProduct() {
-		
+
 	}
-	
+
+	@PostMapping("/insertProduct")
+	public String insertProduct(Product product) {
+		return "redirect:product/getProductList";
+	}
+
+	@GetMapping("/getAuction")
+	public void getAuction() {
+
+	}
+
 }
