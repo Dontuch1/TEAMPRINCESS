9af9cf55488ddb7fb24028dd6a29376ca9a7af65
Dontuch1 <142956326+Dontuch1@users.noreply.github.com>
2023-10-18 15:33:25 +0900

    당근나라번개공주
    version 1.0

---
+++ src/test/java/com/princess/TeamPrincessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.princess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamPrincessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
