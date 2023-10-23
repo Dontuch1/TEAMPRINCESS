3e9d5ff6370ae7edf0086b0d1df45551c89c5c96
eundy <eundy@cheese>
2023-10-23 17:23:24 +0900

    10/23 17:23

--- src/main/java/com/princess/controller/MyPageController.java
+++ src/main/java/com/princess/controller/MyPageController.java
@@ -1,45 +1,65 @@
 package com.princess.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.princess.domain.Member;
+import com.princess.service.MypageService;
+
 @Controller
-@RequestMapping("/mypage")
+@RequestMapping("/mypage/")
 public class MyPageController {
+
+	@Autowired
+	MypageService myService;
 
 	@RequestMapping("/myPageMain")
 	public void myPageMain() {
-		
+
 	}
-	
+
 	@GetMapping("/myDetails")
 	public void myDetails() {
-		
+
 	}
-	
+
+	@PostMapping("/updateDetails")
+	public String updateDetails(Member member) {
+		myService.updateMember(member);
+		return "forward:myDetails";
+	}
+
 	@GetMapping("/myPostList")
 	public void myPostList() {
-		
+
 	}
+
 	@GetMapping("/myProductList")
 	public void myProductList() {
-		
+
 	}
+
 	@GetMapping("/myReviewList")
 	public void myReviewList() {
-		
+
 	}
+
 	@GetMapping("/myWishList")
 	public void myWishList() {
-		
+
 	}
+
 	@GetMapping("/myDeposit")
 	public void myDeposit() {
-		
+
 	}
-	
+
+	@PostMapping("/updateDeposit")
+	public String updateDeposit(Member member) {
+		myService.updateMember(member);
+		return "forward:myDeposit";
+	}
 }
-
-
-
--- src/main/java/com/princess/service/MypageServiceImpl.java
+++ src/main/java/com/princess/service/MypageServiceImpl.java
@@ -6,45 +6,67 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.princess.domain.Board;
-import com.princess.domain.LikeWish;
 import com.princess.domain.Member;
+import com.princess.domain.Product;
 import com.princess.domain.Review;
-import com.princess.persistence.BoardRepository;
-import com.princess.persistence.ProductRepository;
-
+import com.princess.persistence.MemberRepository;
 
 @Service
-public class MypageServiceImpl{
+public class MypageServiceImpl implements MypageService {
+
+
+//	@Autowired
+//	private BoardRepository boardRepo;
 
 	@Autowired
-	private ProductRepository productRepo;
-	@Autowired
-	private BoardRepository boardRepo;
-
+	private MemberRepository memberRepo;
+	
 	// 회원정보 가져오기
+	@Override
 	public Member getMember(Member member) {
 		return null;
 	}
-	// 회원정보 수정 
+
+	// 회원정보 수정 //예치금 수정
+	@Override
 	public void updateMember(Member member) {
-	
+		Member findMember = memberRepo.findById(member.getId()).get();
+		findMember.setNickName(member.getNickName());
+		findMember.setDeposit(member.getDeposit());
+		findMember.setEmail(member.getEmail());
+		findMember.setPhone(member.getPhone());
+		findMember.setUserName(member.getUserName());
+		
+		memberRepo.save(findMember);
 	}
+
 	// 내 게시글 리스트
-	public List<Board> getBoardList(Member member){
+	@Override
+	public List<Board> getBoardList(Member member) {
 		return null;
 	}
-	// 내 등록 상품 리스트 >> 상품서비스임플에서 가져오기 
-	// 내가 받은 후기 
-	public List<Review> getReviewList(Member member){
+
+	// 내 등록 상품 리스트 >> 상품서비스임플에서 가져오기
+	// 내가 받은 후기
+	@Override
+	public List<Review> getReviewList(Member member) {
+
 		return null;
 	}
+
 	// 찜 목록
-	public List<LikeWish> getLikeWishList(Member member){
+	@Override
+	public List<Product> getLikeWishList(Member member) {
+
+	// BooleanBuilder builder = new BooleanBuilder();
+//
+//		QLikeWish qLikeWish = QLikeWish.likeWish;
+//
+//		Pageable pageable = PageRequest.of(0, 10, Sort.Direction.DESC, "pNo");
+//		
+		member.getLikeWishList();
 		return null;
-	}
-	// 예치금 수정
-	public void updateDeposit(Member member) {
-	
+
 	}
 
 
