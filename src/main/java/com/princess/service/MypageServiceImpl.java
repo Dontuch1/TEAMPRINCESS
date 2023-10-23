ae0e441220c1daa806aa3daa19b5e2e97c12b96f
eundy <eundy@cheese>
2023-10-23 09:45:07 +0900

    10/23 09:45

--- src/main/java/com/princess/service/MypageServiceImpl.java
+++ src/main/java/com/princess/service/MypageServiceImpl.java
@@ -2,17 +2,12 @@ package com.princess.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.princess.domain.CheckCondition.Display;
 import com.princess.domain.Product;
-import com.princess.domain.QProduct;
 import com.princess.domain.Search;
 import com.princess.persistence.ProductRepository;
-import com.querydsl.core.BooleanBuilder;
 
 
 @Service
