0c2b04037a25b5cedfae428a5b0f9ddde70e5bef
eundy <eundy@192.168.219.104>
2023-10-29 20:05:59 +0900

    10/29 20:05 배터리, 마이페이지 수정

--- src/main/java/com/princess/config/SecurityConfig.java
+++ src/main/java/com/princess/config/SecurityConfig.java
@@ -39,7 +39,5 @@ public class SecurityConfig {
    public PasswordEncoder passwordEncoder() {
       return PasswordEncoderFactories.createDelegatingPasswordEncoder();
    }
-   
-
-
+  
 }
--- src/main/java/com/princess/controller/SecurityController.java
+++ src/main/java/com/princess/controller/SecurityController.java
@@ -10,3 +10,4 @@ public class SecurityController {
 	public void login() {
 	}
 }
+
