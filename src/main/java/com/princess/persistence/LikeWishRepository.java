1da396ecdd701f8a79f98904a1002ff9919b1913
HoYng <HoYng@Cyclamen>
2023-11-02 20:56:21 +0900

    11/2 20:55 (호영) buyProduct 시 무조건 썬더페이 나가게 수정, updateProduct 구현, insertWish 구현, getProduct에 wishCnt 기능 구현

--- src/main/java/com/princess/persistence/LikeWishRepository.java
+++ src/main/java/com/princess/persistence/LikeWishRepository.java
@@ -11,5 +11,14 @@ import com.princess.domain.Member;
 import com.princess.domain.Product;
 
 public interface LikeWishRepository extends CrudRepository<LikeWish, Long>, QuerydslPredicateExecutor<Product> {
+	
 	List<LikeWish> findByLikeIdAndType(Member member, Type type);
+	
+	List<LikeWish> findBypNoAndType(Long pNo, Type type);
+	
+	void deleteByLikeId(LikeWish likeWish);
+	
+	LikeWish findBypNoAndTypeAndLikeId(Long pNo, Type type, Member member);
+	
+	int countBypNoAndType(Long pNo, Type type);
 }
