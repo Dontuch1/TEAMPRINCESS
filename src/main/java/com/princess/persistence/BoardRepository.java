f1f0bdc72cceebd7d873eba48d92b0d71635e04a
HoYng <HoYng@Cyclamen>
2023-11-07 01:09:35 +0900

    11/7 01:09 (호영) 마이페이지 게시글 display Y만 나오게 변경

--- src/main/java/com/princess/persistence/BoardRepository.java
+++ src/main/java/com/princess/persistence/BoardRepository.java
@@ -7,11 +7,12 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 import com.princess.domain.Board;
+import com.princess.domain.CheckCondition.Display;
 import com.princess.domain.Member;
 
 public interface BoardRepository extends CrudRepository<Board, Long>, QuerydslPredicateExecutor<Board>{
 
-	Page<Board> findByUserId(Member member, Pageable pageable);
+	Page<Board> findByUserIdAndDisplay(Member member, Pageable pageable, Display Y);
 	
 	@Query("SELECT b FROM Board b")
 	Page<Board> getBoardList(Pageable pageable);
